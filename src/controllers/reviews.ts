1d8f9143fc77ea2d8a2c006d99e3e1355bda86ff
Mohamed Ashraf <mohamedashrafothman@gmail.com>
2024-11-21 01:41:42 +0200

    wip: added get single review, delete, and restore reviews

--- src/controllers/reviews.ts
+++ src/controllers/reviews.ts
@@ -69,18 +69,6 @@ export const validator = (method: "create" | "update"): ValidationChain[] => {
 						"Rating must an integer an integer greater than or equal 0 and less than or equal 5!"
 					)
 					.toInt(),
-				body("product")
-					.optional()
-					.isMongoId()
-					.withMessage("Invalid product id!")
-					.notEmpty()
-					.withMessage("Product is required!"),
-				body("order")
-					.optional()
-					.isMongoId()
-					.withMessage("Invalid order id!")
-					.notEmpty()
-					.withMessage("Order is required!"),
 			];
 		default:
 			return [];
@@ -258,24 +246,180 @@ export const getReviewsForProduct = async (
 	// TODO: Implement get reviews for product functionality.
 };
 
+/**
+ * @summary Retrieves a single review by identifier.
+ * @description Fetches a review based on the provided identifier, which can be either a slug or an ObjectId.
+ * Handles errors and returns the review data if found.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} req.params - URL parameters for the request.
+ * @param {String} req.params.review - The review identifier, either a slug or an ObjectId.
+ * @param {Object} res - Express response object.
+ * @param {Function} next - Express next middleware function to handle errors.
+ *
+ * @returns {Object} 200 - Success response with the review data.
+ *   * @property {Object} entities.data - The retrieved review object.
+ * @throws {Error} 401 - Returns an error if the user is not authenticated.
+ * @throws {Error} 404 - Returns an error if no review is found.
+ * @throws {Error} 500 - Returns an error if the review retrieval fails.
+ */
 export const getSingleReview = async (
 	req: Request<{ review: string }, FormatResponseObjectType<IReviewDocument, HttpStatus["OK"]>>,
 	res: Response<FormatResponseObjectType<IReviewDocument, HttpStatus["OK"]>>,
 	next: NextFunction
 ): Promise<void> => {
-	// TODO: Implement get single review functionality.
+	// Check if user logged in
+	if (
+		req.isUnauthenticated() ||
+		!req.user ||
+		![vars.auth.roles.superAdmin, vars.auth.roles.admin].includes(req.user.role)
+	) {
+		const error = createError(httpStatus.UNAUTHORIZED);
+		return next({ ...(error || {}), status: error.status });
+	}
+
+	// Retrieve the review ID from the request parameters
+	const { review: reviewIdentifier } = req.params || {};
+
+	// Attempt to retrieve a review from the database with the given ID,
+	// and if there was an error or no review was found, return the error and end the request
+	const [reviewError, review] = await to(
+		Review.findOne({ _id: reviewIdentifier }).populate({ path: "user" })
+	);
+	if (reviewError || !review) return next(reviewError);
+
+	// Return the retrieved review in the response
+	res.status(httpStatus.OK).json(
+		formatResponseObject({ status: httpStatus.OK, entities: { data: review } })
+	);
 };
 
+/**
+ * @summary Updates a single review by its ID.
+ * @description This method updates a review's details, including its comment and rating.
+ * The method handles errors and returns a success response when the review is successfully updated.
+ *
+ * @param {Object} req - Express request object.
+ * @param {String} req.params.review - The ID of the review to update.
+ * @param {Object} req.body - The updated review data. Optionally includes `comment` and `rating` fields.
+ * @param {String} [req.body.comment] - The updated comment of the review.
+ * @param {String} [req.body.rating] - The updated rating of the review.
+ * @param {Object} res - Express response object.
+ * @param {Function} next - Express next middleware function to handle errors.
+ *
+ * @returns {Object} 200 - Success response indicating the review was updated.
+ *   * @property {Object} entities.data - The retrieved updated review object.
+ * @throws {Error} 401 - Returns an error if the user is not authenticated.
+ * @throws {Error} 404 - If no review is found with the provided identifier.
+ * @throws {Error} 500 - If an error occurs during the update process.
+ */
 export const updateSingleReview = async (
 	req: Request<
 		{ review: string },
 		FormatResponseObjectType<IReviewDocument, HttpStatus["OK"]>,
-		Partial<Pick<IReview, "comment" | "rating" | "product">>
+		Partial<Pick<IReview, "comment" | "rating">>
 	>,
 	res: Response<FormatResponseObjectType<IReviewDocument, HttpStatus["OK"]>>,
 	next: NextFunction
 ): Promise<void> => {
-	// TODO: Implement update single review functionality.
+	// Start a transaction to ensure data integrity
+	const session: ClientSession = await mongoose.startSession();
+	session.startTransaction();
+
+	// Check if user logged in
+	if (req.isUnauthenticated() || !req.user) {
+		const error = createError(httpStatus.UNAUTHORIZED);
+		handleTransactionError(session);
+		return next({ ...(error || {}), status: error.status });
+	}
+
+	// Retrieve the review ID from the request parameters
+	const { review: reviewIdentifier } = req.params || {};
+
+	// Destructure the request body to get comment, and rating data
+	const { comment, rating } = req.body;
+
+	// Check if the review is found, and if there was an error,
+	// return the error and end the request
+	const [reviewError, reviewExists] = await to(
+		Review.findOne({
+			_id: reviewIdentifier,
+			...([vars.auth.roles.user].includes(req.user.role) ? { user: req.user._id } : {}),
+		}).session(session)
+	);
+	if (reviewError || !reviewExists) {
+		handleTransactionError(session);
+		return next(reviewError);
+	}
+
+	// Get review product data, and if there was an error,
+	// return the error and end the request
+	const [productError, productExists] = await to(
+		Product.findOne({ _id: reviewExists.product }).session(session)
+	);
+	if (productError || !productExists) {
+		handleTransactionError(session);
+		return next();
+	}
+
+	// Merge the old review data with the new review comment or rating
+	const newReview = Object.assign(reviewExists, {
+		...(comment && { comment }),
+		...(rating && { rating }),
+	});
+	// Get the product reviews from the product
+	let productReviews = [
+		...(productExists.reviews?.map((review) => review?._id?.toString() || review?.toString()) ||
+			[]),
+	] as string[];
+	// Find the index of the review in the product reviews array
+	const productReviewIndex = productReviews.indexOf(reviewExists?._id?.toString());
+
+	// Save the updated review to the database, and if there is an error during saving,
+	// pass the error to the next middleware
+	const [saveReviewError, updatedReview] = await to(newReview.save({ session }));
+	if (saveReviewError) {
+		handleTransactionError(session);
+		return next(saveReviewError);
+	}
+
+	if (productReviewIndex <= -1) {
+		handleTransactionError(session);
+		return next();
+	}
+
+	// Merge the old product reviews data with the new product review id
+	productReviews = [
+		...(productReviews.slice(0, productReviewIndex) || []),
+		reviewExists._id,
+		...(productReviews.slice(productReviewIndex + 1) || []),
+	] as string[];
+	const newProduct = Object.assign(productExists, {
+		reviews: productReviews,
+	}) as IProductDocument;
+
+	// Save the updated product to the database, and if there is an error during saving,
+	// pass the error to the next middleware
+	const [saveProductError] = await to(newProduct.save({ session }));
+	if (saveProductError) {
+		handleTransactionError(session);
+		return next(saveProductError);
+	}
+
+	// Commit the transaction
+	await session.commitTransaction();
+	session.endSession();
+
+	// Set a flash message to indicate that the review was updated successfully,
+	// and return the updated review in the response
+	req.flash("success", "Review updated successfully.");
+	res.status(httpStatus.OK).json(
+		formatResponseObject({
+			status: httpStatus.OK,
+			entities: { data: updatedReview },
+			flashes: req.flash(),
+		})
+	);
 };
 
 /**
